8eb2ffedd6476f0ac3b3b6120441ee0fcaab6f5c
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-16 15:39:57 +0600

    New api view added for patching item

--- backend/rest_api/apiviews.py
+++ backend/rest_api/apiviews.py
@@ -6,10 +6,28 @@ from . serializers import ToDoSerializer
 from . models import ToDo
 
 
-class ToDoApiView(APIView):
+class DeleteApiView(APIView):
     def delete(self, request, timestamp):
         item = ToDo.objects.get(timestamp=timestamp)
         item.delete()
 
         return Response({'message': 'Delete Successful'},
+                        status=status.HTTP_200_OK)
+
+
+
+class PatchApiView(APIView):
+    def patch(self, request, timestamp, patch_name):
+        data = request.data
+        patch_file = data[patch_name]
+        item = ToDo.objects.get(timestamp=timestamp)
+
+        if patch_name == 'name':
+            item.name = patch_file
+        else:
+            item.done = patch_file
+
+        item.save()
+
+        return Response({'message': 'Update Successful'},
                         status=status.HTTP_200_OK)
