a92c8ed343183bc1033ee138c81acd0454e317ee
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-16 15:40:41 +0600

    Minor styling fix. Added a new check button to toggle item status

--- frontend/src/Lists/Lists.js
+++ frontend/src/Lists/Lists.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import './Lists.css'
+import React from "react";
+import "./Lists.css";
 
 const list_items = (props) => {
-
   return (
-      <li><button className="remove-item-btn" value={props.value} onClick={props.click}>x</button>{props.name}</li>
-  )
-}
+    <li>
+      <button
+        className="fa fa-check"
+        style={{ fontSize: "20px", outline: "none" }}
+        value={props.value}
+        onClick={props.toggleStatus}
+      ></button>
+      <button
+        className="fa fa-edit"
+        style={{ fontSize: "20px", outline: "none" }}
+        value={props.value}
+        onClick={props.edit}
+      ></button>
+      <button
+        className="fa fa-trash mr-2"
+        style={{ fontSize: "20px", outline: "none" }}
+        value={props.value}
+        onClick={props.delete}
+      ></button>
 
+      <span style={props.style}>{props.name}</span>
+    </li>
+  );
+};
 
-export default list_items
+export default list_items;
