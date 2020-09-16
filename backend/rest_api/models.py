46aa9ff2f722130d400bbb444bdac5376777dcc5
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-16 15:39:41 +0600

    Todo model modified.
    Extra field named done added to keep track if the item is done or not

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -6,6 +6,12 @@ from django.db import models
 class ToDo(models.Model):
     name = models.CharField(max_length=255)
     timestamp = models.CharField(max_length=32)
+    CHOICE = {
+    ('', 'FALSE'),
+    ('line-through', 'TRUE')
+    }
+
+    done = models.CharField(default='', choices=CHOICE, max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.name
